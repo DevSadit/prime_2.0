dea5ff15adbbd8f97983abfa404f57768f97b505
DevSadit <dev.sadit@gmail.com>
2026-03-27 01:28:48 +0600

    3rd list problem solved

--- day_3_python/assignment.py
+++ day_3_python/assignment.py
@@ -6,10 +6,23 @@
 # else:
 #     print("It is not a palindrome")
 
-# 2.
-nums = [10, 20]
-count = 0
-for i in nums:
-    count += i
+# # 2.
+# nums = [10, 20]
+# count = 0
+# for i in nums:
+#     count += i
 
-print(count)
+# print(count)
+
+# 3.
+# list1 = list(map(int, input("Enter first list elements: ").split()))
+# list2 = list(map(int, input("Enter second list elements: ").split()))
+
+# print("List 1:", list1)
+# print("List 2:", list2)
+
+# merged = list1 + list2
+
+
+# merged.sort()
+# print(merged)
