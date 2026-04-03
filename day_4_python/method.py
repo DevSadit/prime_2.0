f638fd57d2087c2c7bee06f003507d3d17f6e29c
DevSadit <dev.sadit@gmail.com>
2026-04-04 02:15:35 +0600

    class method learned

--- day_4_python/method.py
+++ day_4_python/method.py
@@ -1,6 +1,6 @@
 # there are 3 types of method.
-# 1. isinstance
-# 2. class
+# 1. isinstance -> where the first parameter is - self
+# 2. class -> where the first parameter is - cls
 # 3. static
 
 
@@ -17,9 +17,18 @@ class Laptop:
         print(f"Laptop RAM: {self.ram}")
         print(f"Laptop STORAGE: {self.storage}")
 
+    # making class method
+    @classmethod  # --> decorator
+    def get_storage(cls):
+        print(f"laptop storage: {cls.storage_type}")
+
 
 l1 = Laptop(16, 256)
 l2 = Laptop(32, 512)
 
 
 l1.get_info()
+
+# calling the class method
+l2.get_storage()
+Laptop.get_storage()
